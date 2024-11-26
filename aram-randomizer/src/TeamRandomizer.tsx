32a21e336c46cb0648026c2bf2c7ee3cafdc534b
Condusty <64013051+Condusty@users.noreply.github.com>
2024-11-26 22:22:09 +0100

    Added Player Input field

--- aram-randomizer/src/TeamRandomizer.tsx
+++ aram-randomizer/src/TeamRandomizer.tsx
@@ -1,48 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const getRandomInt = (max: number) => {
-    return Math.floor(Math.random() * max);
-};
+const TeamRandomizer = () => {
+    const [input, setInput] = useState<string>("");
+    const playerList: string[] = [];
+    let team1list: string[] = [];
+    let team2list: string[] = [];
 
-const splitListRandomly = (inputList: string[]): [string[], string[]] => {
-    const list1: string[] = [];
-    const list2: string[] = [];
-    const shuffledList = [...inputList].sort(() => 0.5 - Math.random());
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+    }
 
-    shuffledList.forEach((item, index) => {
-        if (index % 2 === 0) {
-            list1.push(item);
-        } else {
-            list2.push(item);
-        }
-    });
+    const splitListRandomly = (inputList: string[]): [string[], string[]] => {
+        const list1: string[] = [];
+        const list2: string[] = [];
+        const shuffledList = [...inputList].sort(() => 0.5 - Math.random());
 
-    return [list1, list2];
-};
+        shuffledList.forEach((item, index) => {
+            if (index % 2 === 0) {
+                list1.push(item);
+            } else {
+                list2.push(item);
+            }
+        });
 
-// Example usage:
-const inputList = ["a", "b", "c", "d", "e", "f"];
-const [list1, list2] = splitListRandomly(inputList);
+        return [list1, list2];
+    };
 
-const TeamRandomizer = () => (
-    <div>
+    function addPlayerList(input: string) {
+        playerList.push(input);
+        console.log(playerList);
+        input = "";
+    }
+
+    return (
         <div>
-            <h3>Team 1</h3>
-            <ul>
-                {list1.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            <input type="text" placeholder="Enter names" onChange={(e) => handleInputChange(e)} />
+            <button onClick={() => addPlayerList(input)}>
+                Add
+            </button>
+            <button onClick={() => splitListRandomly(playerList)}>
+                Create Random Teams
+            </button>
+            <div>
+                <h3>Team 1</h3>
+                <ul>
+                    {team1list.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h3>Team 2</h3>
+                <ul>
+                    {team2list.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
-        <div>
-            <h3>Team 2</h3>
-            <ul>
-                {list2.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    </div>
-);
+    )
+};
 
 export default TeamRandomizer;
