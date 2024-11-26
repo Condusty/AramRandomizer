9fec6e0e6d6842cf60f58e58a501d50b94eec133
Casimir Bausch <cbausch@mail.uni-paderborn.de>
2024-11-26 10:38:15 +0100

    Added function for Random Teams
    The function splits the list randomly into two evenly big lists

--- aram-randomizer/src/App.tsx
+++ aram-randomizer/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Message from './message'
+import TeamRandomizer from './TeamRandomizer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +26,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Message />
+        <TeamRandomizer />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
