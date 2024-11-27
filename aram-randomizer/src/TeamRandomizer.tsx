bf88ea7844ce58cc3a1432fa294d2bb011472afa
Condusty <64013051+Condusty@users.noreply.github.com>
2024-11-27 14:04:43 +0100

    changed background color

--- aram-randomizer/src/TeamRandomizer.tsx
+++ aram-randomizer/src/TeamRandomizer.tsx
@@ -47,7 +47,7 @@ const TeamRandomizer = () => {
     return (
         <div>
             <input
-                className="rounded-md block mb-2 text-sm font-medium bg-slate-800 text-gray-600 dark:text-white p-2"
+                className="rounded-md mr-2 text-sm font-medium bg-slate-800 text-gray-600 dark:text-white p-2"
                 type="text"
                 placeholder="Enter names"
                 value={input}
@@ -58,22 +58,22 @@ const TeamRandomizer = () => {
             <button type="button" className="text-gray-900 bg-white border border-gray-300 bg-gradient-to-r from-green-400 via-green-500 to-green-600 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 onClick={addPlayerList}>Add
             </button>
-
+            <br />
             <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 onClick={() => splitListRandomly(playerList)}>Create Random Teams</button>
             <div>
-                <h3 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Team 1</span></h3>
+                <h3 className="mt-5 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Team 1</span></h3>
                 <ul>
                     {team1list.map((item, index) => (
-                        <li key={index}>{item}</li>
+                        <li className="text-white" key={index}>{item}</li>
                     ))}
                 </ul>
             </div>
             <div>
-                <h3 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Team 2</span></h3>
+                <h3 className="mt-5 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Team 2</span></h3>
                 <ul>
                     {team2list.map((item, index) => (
-                        <li key={index}>{item}</li>
+                        <li className="text-white" key={index}>{item}</li>
                     ))}
                 </ul>
             </div>
